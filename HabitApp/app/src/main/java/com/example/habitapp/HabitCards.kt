e56c20c1fffb6303619b25bb06a5104f661d91ea
Brandon Devereux <bdevereux@wccnet.edu>
2023-03-27 15:36:55 -0400

    uploading database

--- HabitApp/app/src/main/java/com/example/habitapp/HabitCards.kt
+++ HabitApp/app/src/main/java/com/example/habitapp/HabitCards.kt
@@ -1,14 +1,20 @@
 package com.example.habitapp
 
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+
+
 import androidx.compose.foundation.layout.Row
+
+
+
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,13 +24,13 @@ fun HabitCards(
     viewModel: HabitViewModel
 ){
     Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically
-    ){
+        modifier = modifier){
         Text(
             modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
                 .weight(1f)
-                .padding(start = 16.dp)
-                ,
+                .padding(start = 1.dp),
             text = habitName)
         //deletes habit from database
         Button(onClick = { viewModel.deleteHabit(habitName) }) {
--- HabitApp/app/src/main/java/com/example/habitapp/HabitScreen.kt
+++ HabitApp/app/src/main/java/com/example/habitapp/HabitScreen.kt
@@ -1,14 +1,18 @@
 package com.example.habitapp
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.habitapp.Settings.SettingsScreen
 import com.example.habitapp.Streaks.StreaksScreen
 
@@ -16,24 +20,24 @@ import com.example.habitapp.Streaks.StreaksScreen
 fun HabitScreen(
     modifier: Modifier = Modifier,
     habitViewModel: HabitViewModel
-    ){
+) {
 
     //This is how data is moved between composable functions and database.
     val habitList by habitViewModel.allHabits.observeAsState()
 
     //used to capture text from TextField.
-    var text by remember { mutableStateOf(TextFieldValue(""))}
+    var text by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column(modifier = modifier){
-        Row(modifier = modifier){
+    Column(modifier = modifier, horizontalAlignment = Alignment.Start) {
+        Row(modifier = modifier.padding(1.dp)) {
             TextField(
                 value = text,
-                onValueChange = {newText ->
+                onValueChange = { newText ->
                     text = newText
                 }
             )
             //Adds habits to db
-            Button(onClick = { habitViewModel.addHabitToDB(text.text)} ){
+            Button(onClick = { habitViewModel.addHabitToDB(text.text) }) {
 
             }
         }
@@ -50,6 +54,36 @@ fun HabitScreen(
             SettingsScreen()
         }*/
     }
-    }
 
+    //This is the button and text field alignment
+    Column(modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End,
+    ) {
+        Row(
+            modifier = modifier,
+        ) {
+            Button(
+                onClick = {habitViewModel.addHabitToDB(text.text) },
+                Modifier.padding(8.dp),
+                shape = CircleShape,
+            )
+            {
+                Text(
+                    text = "+",
+                    Modifier.padding(3.dp),
+                    fontSize = 30.sp
+                )
+            }
+        }
+    }
+}
+
+
+/*  TextField(
+      value = text,               //TODO
+      onValueChange = { newText ->
+          text = newText
+      }
+  ) */
 
